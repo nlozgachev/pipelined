3a1299491c464351301e89a211952ba6acac1ce5
Nikita Lozgachev <code@lozgachev.dev>
2026-03-23 04:02:26 +0400

    chore: types config

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./Composition/index.ts";
+export * from "./Core/index.ts";
+export * from "./Types/index.ts";
+export * from "./Utils/index.ts";
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: {
+		index: "src/index.ts",
 		composition: "src/Composition/index.ts",
 		core: "src/Core/index.ts",
 		utils: "src/Utils/index.ts",
