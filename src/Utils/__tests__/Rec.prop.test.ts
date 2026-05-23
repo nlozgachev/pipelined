f23daccbfb48ac2aeb7ce65faf94fe39a45501db
Nikita Lozgachev <code@lozgachev.dev>
2026-05-23 13:39:23 +0400

    test: fix null-proto tests

--- src/Utils/__tests__/Rec.prop.test.ts
+++ src/Utils/__tests__/Rec.prop.test.ts
@@ -2,7 +2,7 @@ import fc from "fast-check";
 import { expect, test } from "vitest";
 import { Rec } from "../Rec.ts";
 
-const dict = fc.dictionary(fc.string({ minLength: 1 }), fc.integer());
+const dict = fc.dictionary(fc.string({ minLength: 1 }), fc.integer()).map((o) => ({ ...o }));
 
 // ---------------------------------------------------------------------------
 // map
